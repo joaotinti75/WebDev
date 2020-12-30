e4554964fa4539dd3512e28e23ba2608ab08ebe4
Joao Tinti <joaotinti75@gmail.com>
2020-12-30 08:10:06 -0300

    Falta atualização automática

---
+++ message_app/public/scripts/msg_script.js
@@ -0,0 +1,3 @@
+var paragrafos = document.querySelectorAll('p')
+var ultimoP = paragrafos[paragrafos.length-1]
+ultimoP.style.backgroundColor = '#2e8a04'
--- message_app/servidor.js
+++ message_app/servidor.js
@@ -4,6 +4,8 @@ const handlebars = require('express-handlebars')
 const bodyParser = require('body-parser')
 const Msg = require('./models/Mensagem')
 
+app.use(express.static('public'))
+
 // Configurando o Template Engine Handlebars
 app.engine('handlebars', handlebars({defaultLayout:'main'}))
 app.set('view engine', 'handlebars')
@@ -19,36 +21,24 @@ app.get('/', function(req, res){
 app.get('/sucesso', function(req, res){
     Msg.findAll().then(function(msgs){
         //console.log(msgs) //trata-se de um array
-        res.render('sucesso', {msgs:msgs})
+        res.render('formulario', {msgs:msgs})
     })
 })
 
-//Para eu ordenar as mensagens do mais recente para o mais antigo, basta eu inserir alguns argumentos na função findAll
-
-//Msg.findAll({order: [['id', 'DESC']]})
 
 app.post('/add', function(req, res){
     Msg.create({
         msg: req.body.mensagem
     }).then(function(){
         res.redirect('/sucesso')
-        //res.send('Post criado com sucesso')
+        //res.send('Post criado com sucesso') 
     }).catch(function(erro){
         res.send('Houve um erro: ' + erro)
     })
     //res.send(`Mensagem: ${req.body.mensagem}`)//Quando o formulario tem o método post, devemos mudar o tipo da rota para post
 })
 
-app.get('/deletar/:id', function(req,res){
-    Msg.destroy({where: {'id':req.params.id}}).then(function(){
-        res.send('Mensagem deletada com sucesso')
-    }).catch(function(erro){
-        res.send('Esta postagem não existe!')
-    })
-}) //deleto uma linha do banco através da id da linha
-
-
-
-app.listen(8081, function(){
-    console.log('Servidor rodando na url: http://localhost:8081')
+app.listen('80', '192.168.1.30', () => {
+    console.info('server started on port 80')
 })
+
